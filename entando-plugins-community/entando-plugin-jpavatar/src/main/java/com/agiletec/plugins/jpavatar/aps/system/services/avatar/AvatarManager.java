a75252fc04cf78451fe6f3bd9abed6b666197af2
eugeniosant <eugenio.santoboni@gmail.com>
2012-03-21 17:28:43 +0100

    License upgrade

--- entando-plugins-community/entando-plugin-jpavatar/src/main/java/com/agiletec/plugins/jpavatar/aps/system/services/avatar/AvatarManager.java
+++ entando-plugins-community/entando-plugin-jpavatar/src/main/java/com/agiletec/plugins/jpavatar/aps/system/services/avatar/AvatarManager.java
@@ -1,20 +1,20 @@
 /*
- *
- * Copyright 2009 AgileTec s.r.l. (http://www.agiletec.it) All rights reserved.
- *
- * This file is part of jAPS software.
- * jAPS is a free software; 
- * you can redistribute it and/or modify it
- * under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; version 2.
- * 
- * See the file License for the specific language governing permissions   
- * and limitations under the License
- * 
- * 
- * 
- * Copyright 2009 AgileTec s.r.l. (http://www.agiletec.it) All rights reserved.
- *
- */
+*
+* Copyright 2012 Entando S.r.l. (http://www.entando.com) All rights reserved.
+*
+* This file is part of Entando software.
+* Entando is a free software; 
+* you can redistribute it and/or modify it
+* under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; version 2.
+* 
+* See the file License for the specific language governing permissions   
+* and limitations under the License
+* 
+* 
+* 
+* Copyright 2012 Entando S.r.l. (http://www.entando.com) All rights reserved.
+*
+*/
 package com.agiletec.plugins.jpavatar.aps.system.services.avatar;
 
 import java.io.File;
@@ -35,6 +35,9 @@ import com.agiletec.plugins.jpavatar.aps.system.utils.MD5Util;
 import com.agiletec.plugins.jpuserprofile.aps.system.services.profile.IUserProfileManager;
 import com.agiletec.plugins.jpuserprofile.aps.system.services.profile.model.UserProfile;
 
+/**
+ * @author S.Puddu
+ */
 @Aspect
 public class AvatarManager extends AbstractService implements IAvatarManager {
 
--- entando-plugins-community/entando-plugin-jpavatar/src/test/java/com/agiletec/plugins/jpavatar/apsadmin/ApsAdminPluginBaseTestCase.java
+++ entando-plugins-community/entando-plugin-jpavatar/src/test/java/com/agiletec/plugins/jpavatar/apsadmin/ApsAdminPluginBaseTestCase.java
@@ -1,9 +1,9 @@
 /*
 *
-* Copyright 2005 AgileTec s.r.l. (http://www.agiletec.it) All rights reserved.
+* Copyright 2012 Entando S.r.l. (http://www.entando.com) All rights reserved.
 *
-* This file is part of jAPS software.
-* jAPS is a free software; 
+* This file is part of Entando software.
+* Entando is a free software; 
 * you can redistribute it and/or modify it
 * under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; version 2.
 * 
@@ -12,7 +12,7 @@
 * 
 * 
 * 
-* Copyright 2005 AgileTec s.r.l. (http://www.agiletec.it) All rights reserved.
+* Copyright 2012 Entando S.r.l. (http://www.entando.com) All rights reserved.
 *
 */
 package com.agiletec.plugins.jpavatar.apsadmin;
@@ -21,11 +21,6 @@ import com.agiletec.ConfigTestUtils;
 import com.agiletec.apsadmin.ApsAdminBaseTestCase;
 import com.agiletec.plugins.jpavatar.PluginConfigTestUtils;
 
-/**
- * BaseTestCase Class for jpuserprofile apsadmin tests.
- * You have to comment "getConfigUtils()" 
- * Methods for testing plugin into custom jAPS 2.0 project.
- */
 public class ApsAdminPluginBaseTestCase extends ApsAdminBaseTestCase {
 	
 	@Override
