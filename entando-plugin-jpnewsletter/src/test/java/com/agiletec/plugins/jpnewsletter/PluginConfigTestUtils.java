ced516307984f7604c56e6b1e7638bb33682ca63
eugeniosant <eugenio.santoboni@gmail.com>
2013-02-04 17:46:48 +0100

    Porting to component architecture of "Newsletter" plugin

--- entando-plugin-jpnewsletter/src/test/java/com/agiletec/plugins/jpnewsletter/PluginConfigTestUtils.java
+++ entando-plugin-jpnewsletter/src/test/java/com/agiletec/plugins/jpnewsletter/PluginConfigTestUtils.java
@@ -26,12 +26,13 @@ public class PluginConfigTestUtils extends ConfigTestUtils {
 	
 	@Override
 	protected String[] getSpringConfigFilePaths() {
-    	String[] filePaths = new String[5];
-		filePaths[0] = "classpath:spring/systemConfig.xml";
-		filePaths[1] = "classpath:spring/aps/**/**.xml";
-		filePaths[2] = "classpath:spring/apsadmin/**/**.xml";
-		filePaths[3] = "classpath*:spring/plugins/**/aps/**/**.xml";
-		filePaths[4] = "classpath*:spring/plugins/**/apsadmin/**/**.xml";  	
+    	String[] filePaths = new String[6];
+		filePaths[0] = "classpath:spring/propertyPlaceholder.xml";
+		filePaths[1] = "classpath:spring/baseSystemConfig.xml";
+		filePaths[2] = "classpath*:spring/aps/**/**.xml";
+		filePaths[3] = "classpath*:spring/apsadmin/**/**.xml";
+		filePaths[4] = "classpath*:spring/plugins/**/aps/**/**.xml";
+		filePaths[5] = "classpath*:spring/plugins/**/apsadmin/**/**.xml";
 		return filePaths;
     }
 	
